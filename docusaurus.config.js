c5c8a3acaad1025aa7b1a4cf3b9c5ef8bfd4dad4
ZhangJie <jay@ZhangJies-MacBook-Pro.local>
2026-01-18 15:11:51 +0800

    011806

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,18 +20,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'git@github.com:buhaoqi/buhaoqi.github.io.git',
+  url: 'https://buhaoqi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-  
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'buhaoqi', // Usually your GitHub org/user name.
   projectName: 'buhaoqi', // Usually your repo name.
   deploymentBranch: 'gh-pages',
-
+  
 
   onBrokenLinks: 'throw',
 
