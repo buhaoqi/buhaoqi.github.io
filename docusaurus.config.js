ea7cb0e8ae7735a274fd3039b93a21c3b14e35c2
buhaoqi <buhaoqi@qq.com>
2026-01-29 18:53:54 +0800

    012911

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/logo-orange-social.png',
       colorMode: {
         respectPrefersColorScheme: true,
       },
